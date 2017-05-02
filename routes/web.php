e8dbe5810e508df8b11cb6b2d7ad2f23a8194c7d
RamintaRam <raminta5@yahoo.com>
2017-05-02 14:21:34 +0300

    some changes

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'cheese'], function ()
+{
+    Route::get('/create', function ()
+    {
+        return view('cheese');
+    });
+
+    Route::post('/create', ['as' => 'app.cheese.create', 'uses' => 'CAPizzaCheeseController@create']);
+});
+
+
+Route::group(['prefix' => 'pad'], function ()
+{
+    Route::get('/create', function ()
+    {
+        return view('pad');
+    });
+
+    Route::post('/create', ['as' => 'app.pad.create', 'uses' => 'CAPizzaPadController@create']);
+});
+
+
+
+
+
+
+
+Route::group(['prefix' => 'pizza'], function ()
+{
+    Route::get('/create', ['uses' => 'CACheeseController@showCreate']);
+
+    Route::post('/create', ['as' => 'app.people.create', 'uses' => 'HOBPeopleController@create']);
+});
+
